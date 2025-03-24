486d9d03056887cdab83e449efc5220157e57f68
notioncn <88926151+notioncn@users.noreply.github.com>
2025-03-24 20:38:01 +0800

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ['images.unsplash.com'],
+    unoptimized: true,
   },
 }
 
-module.exports = nextConfig 
+module.exports = nextConfig
